83dcf5731c39ce5fe18d0ddd2c2d0fb9a0a4c212
Joshua <krealsion@gmail.com>
2019-11-06 11:46:30 -0700

    Timer Creation

---
+++ include/Timer.h
@@ -0,0 +1,65 @@
+//
+// Created by jdemoss on 11/4/19.
+//
+
+#ifndef LEAVINGTERRA_TIMER_H
+#define LEAVINGTERRA_TIMER_H
+
+#include <atomic>
+#include <chrono>
+
+class Timer {
+public:
+    static void UpdateTime();
+    /**
+     * This sets whether UpdateTime() will be called.
+     * If set to false, each check of IsTime() will update the CurrentTime
+     * Not recommended for situations where order of IsTime() fires are important
+     * @param ManualCalls True for calling UpdateTime() manually, False for Automatic CurrentTime Updates
+     */
+    static void SetManualUpdateManagement(bool ManualCalls);
+
+    Timer(double Delay);
+
+    /**
+     * returns true if it is time to handle a tick
+     * Only use this if the tick will be handled after the call
+     * @return a bool representing if it is time for an tick
+     */
+    bool IsTime();
+    /**
+     * returns true if it is time to handle a tick
+     * Use this if the tick will not be handled by the call
+     * @return a bool representing if it is time for an tick
+     */
+    bool PeekIsTime();
+    /**
+     * @return a double representing the progress in milliseconds so far
+     */
+    double PeekProgress();
+    /**
+     * @return a double representing the progress in percent form. (0 = 0%, 1 = 100%)
+     */
+    double PeekProgressPercentage();
+
+    double GetTimeMultiplier();
+    void SetTimeMultiplier(double TimeMultiplier);
+
+    bool IsPaused();
+    void Pause();
+    void Resume();
+
+private:
+    static std::chrono::time_point<std::chrono::steady_clock> CurrentTime;
+    static bool ManualCalls;
+
+    std::chrono::nanoseconds GetEffectiveDelay();
+    void SwitchPauseState();
+
+    std::chrono::time_point<std::chrono::steady_clock> LastUpdate;
+    double Delay;
+    double TimeMultiplier;
+    bool Paused;
+};
+
+#endif //LEAVINGTERRA_TIMER_H
---
+++ src/Timer.cpp
@@ -0,0 +1,99 @@
+//
+// Created by jdemoss on 11/4/19.
+//
+
+#include "../include/Timer.h"
+
+std::chrono::time_point<std::chrono::steady_clock> Timer::CurrentTime = std::chrono::steady_clock::now();
+bool Timer::ManualCalls = true;
+
+void Timer::UpdateTime() {
+    CurrentTime = std::chrono::steady_clock::now();
+}
+
+void Timer::SetManualUpdateManagement(bool ManualCalls) {
+    Timer::ManualCalls = ManualCalls;
+}
+
+Timer::Timer(double Delay) {
+    this->Delay = Delay;
+    TimeMultiplier = 1;
+    LastUpdate = CurrentTime;
+    Paused = false;
+}
+
+bool Timer::IsTime() {
+    if (PeekIsTime()) {
+        LastUpdate += GetEffectiveDelay();
+        return true;
+    }
+    return false;
+}
+
+bool Timer::PeekIsTime() {
+    if (Paused) {
+        return false;
+    }
+    if (!ManualCalls) {
+        UpdateTime();
+    }
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(CurrentTime - LastUpdate) >= GetEffectiveDelay();
+}
+
+double Timer::PeekProgress() {
+    if (Paused) {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(LastUpdate.time_since_epoch()).count() / 1000000.0;
+    }
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(CurrentTime - LastUpdate).count() / 1000000.0;
+}
+
+double Timer::PeekProgressPercentage() {
+    return PeekProgress() / std::chrono::duration_cast<std::chrono::nanoseconds>(GetEffectiveDelay()).count() * 1000000;
+}
+
+double Timer::GetTimeMultiplier() {
+    return TimeMultiplier;
+}
+
+void Timer::SetTimeMultiplier(double TimeMultiplier) {
+    if (Paused) {
+        LastUpdate = LastUpdate - std::chrono::nanoseconds((long) (
+                LastUpdate.time_since_epoch().count() * (1 - (this->TimeMultiplier / TimeMultiplier))));
+    } else {
+        //Subtract time to set the relative time progress to an equal percentage
+        LastUpdate = CurrentTime - std::chrono::nanoseconds((long) (
+                (CurrentTime.time_since_epoch().count() - LastUpdate.time_since_epoch().count()) *
+                this->TimeMultiplier / TimeMultiplier));
+    }
+    this->TimeMultiplier = TimeMultiplier;
+}
+
+bool Timer::IsPaused() {
+    return Paused;
+}
+
+void Timer::Pause() {
+    if (!Paused) {
+        SwitchPauseState();
+    }
+}
+
+void Timer::Resume() {
+    if (Paused) {
+        SwitchPauseState();
+    }
+}
+
+std::chrono::nanoseconds Timer::GetEffectiveDelay() {
+    return std::chrono::nanoseconds((long) (Delay / TimeMultiplier * 1000000));
+}
+
+/**
+ * Internal Function
+ * Swaps the Pause state by either storing or restoring the time progressed since the Pause
+ */
+void Timer::SwitchPauseState() {
+    Paused = !Paused;
+    LastUpdate = CurrentTime - LastUpdate.time_since_epoch();
+}
+
