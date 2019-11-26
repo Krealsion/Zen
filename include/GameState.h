2feedbe4f19f8db35edb5d6853be4e5243bf9b2c
Joshua <Krealsion@gmail.com>
2019-11-25 21:12:04 -0700

    Formatting fix

--- include/GameState.h
+++ include/GameState.h
@@ -5,15 +5,14 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 
-#include "GameGraphics.h"
-
 class GameStateManager;
+#include "GameGraphics.h"
 
 class GameState {
 public:
     virtual ~GameState() = default;
     virtual void Update() = 0;
-    virtual void Draw(GameGraphics &G) = 0;
+    virtual void Draw(GameGraphics &g) = 0;
     virtual void Pause() {}
     virtual void Resume() {}
     virtual void SetGameStateManager(GameStateManager* StateManager){Manager = StateManager;}
